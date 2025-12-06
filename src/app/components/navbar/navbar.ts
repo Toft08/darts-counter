a5c5607051e1c273fd385de8838f94704dc44c88
Toft <toft.dah@gmail.com>
2025-12-06 10:51:59 +0200

    move input option to navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { filter } from 'rxjs/operators';
+import { GameService } from '../../services/game';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.scss'],
   standalone: true,
-  imports: [RouterLink]
+  imports: [RouterLink, CommonModule, FormsModule]
 })
 export class NavbarComponent {
   currentGameMode: string = '';
+  showSettings: boolean = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public game: GameService) {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: any) => {
@@ -25,5 +29,17 @@ export class NavbarComponent {
         this.currentGameMode = '';
       }
     });
+  }
+
+  toggleSettings() {
+    this.showSettings = !this.showSettings;
+  }
+
+  closeSettings() {
+    this.showSettings = false;
+  }
+
+  onInputModeChange() {
+    this.game.resetRound();
   }
 }
